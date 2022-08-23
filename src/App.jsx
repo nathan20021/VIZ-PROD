8cd3aa753f349325635fd43610d2b5014c9c1333
Nathan Luong <nathan@jamlabs.com>
2022-08-23 03:14:04 -0400

    Added init states

--- src/App.jsx
+++ src/App.jsx
@@ -56,13 +56,172 @@ const toImageNameFromURL = (url) => {
 };
 const listOfBoundaries = Object.keys(boundaryJson);
 
-const initialEdges = [];
+const initialEdges = [
+  {
+    source: "node-7",
+    sourceHandle: "sr",
+    target: "node-8",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-7sr-node-8sl",
+    selected: false,
+  },
+  {
+    source: "node-6",
+    sourceHandle: "sr",
+    target: "node-8",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-6sr-node-8sl",
+    selected: false,
+  },
+  {
+    source: "node-8",
+    sourceHandle: "sr",
+    target: "node-10",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-8sr-node-10sl",
+    selected: false,
+  },
+  {
+    source: "node-10",
+    sourceHandle: "sr",
+    target: "node-13",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-10sr-node-13sl",
+  },
+  {
+    source: "node-8",
+    sourceHandle: "sr",
+    target: "node-9",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-8sr-node-9sl",
+    selected: false,
+  },
+  {
+    source: "node-8",
+    sourceHandle: "sr",
+    target: "node-11",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-8sr-node-11sl",
+    selected: false,
+  },
+  {
+    source: "node-10",
+    sourceHandle: "sr",
+    target: "node-12",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-10sr-node-12sl",
+  },
+  {
+    source: "node-13",
+    sourceHandle: "sr",
+    target: "node-19",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-13sr-node-19sl",
+  },
+  {
+    source: "node-12",
+    sourceHandle: "sr",
+    target: "node-19",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-12sr-node-19sl",
+  },
+  {
+    source: "node-11",
+    sourceHandle: "sr",
+    target: "node-19",
+    targetHandle: "sl",
+    style: {
+      stroke: "#000000",
+    },
+    markerEnd: {
+      type: "arrowclosed",
+      color: "#000000",
+      strokeWidth: "2px",
+    },
+    id: "reactflow__edge-node-11sr-node-19sl",
+  },
+];
 const initialNodes = [
   {
-    id: "node-1",
+    id: "node-0",
     type: "boundaryNode",
     draggable: false,
-    position: { x: 200, y: 200 },
+    position: {
+      x: 210,
+      y: 179.5,
+    },
     zIndex: -1,
     data: {
       nodeId: "node-0",
@@ -70,19 +229,493 @@ const initialNodes = [
       bgColor: "#eeeeee38",
       url: "aws-logo.jpeg",
       dashed: false,
-      width: "700px",
-      height: "500px",
+      width: "400px",
+      height: "300px",
       cornerIcon: true,
       bodySelectable: false,
     },
+    width: 385,
+    height: 255,
+    selected: false,
+    positionAbsolute: {
+      x: 210,
+      y: 179.5,
+    },
+    dragging: false,
   },
   {
     id: "node-2",
     type: "headerNode",
-    draggable: true,
-    position: { x: 200, y: 150 },
+    position: {
+      x: 48.61066942324078,
+      y: 104.86184452423977,
+    },
+    width: 275,
+    height: 35,
+    selected: false,
+    positionAbsolute: {
+      x: 48.61066942324078,
+      y: 104.86184452423977,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-3",
+    type: "boundaryNode",
+    position: {
+      x: -14.990545188792225,
+      y: 234.41724454641815,
+    },
     zIndex: -1,
-    data: {},
+    data: {
+      color: "#001797",
+      bgColor: "none",
+      url: "none",
+      dashed: true,
+      width: "200px",
+      height: "200px",
+      cornerIcon: false,
+      bodySelectable: false,
+      nodeId: "node-3",
+    },
+    width: 205,
+    height: 154,
+    draggable: false,
+    selected: false,
+    positionAbsolute: {
+      x: -14.990545188792225,
+      y: 234.41724454641815,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-6",
+    type: "serviceComponent",
+    position: {
+      x: 39.86592722320245,
+      y: 318.6952172620662,
+    },
+    data: {
+      url: "aws-asset-res/[loT]%20AWS-IoT-Thing/Generic.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 39.86592722320245,
+      y: 318.6952172620662,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-6",
+    type: "serviceComponent",
+    position: {
+      x: 39.86592722320245,
+      y: 318.6952172620662,
+    },
+    data: {
+      url: "aws-asset-res/[loT]%20AWS-IoT-Thing/Medical-Emergency.png",
+    },
+    selected: false,
+    positionAbsolute: {
+      x: 39.86592722320245,
+      y: 318.6952172620662,
+    },
+    dragging: false,
+    width: 30,
+    height: 30,
+  },
+  {
+    id: "node-7",
+    type: "serviceComponent",
+    position: {
+      x: 36.85477445790053,
+      y: 250.79371771119628,
+    },
+    data: {
+      url: "aws-asset-res/[loT]%20AWS-IoT-Thing/Generic.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 36.85477445790053,
+      y: 250.79371771119628,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-8",
+    type: "serviceComponent",
+    position: {
+      x: 116.69863574367338,
+      y: 286.1450298182074,
+    },
+    data: {
+      url: "aws-asset-res/[Migration-and-Transfer]%20AWS-Mainframe-Modernization/Developer.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 116.69863574367338,
+      y: 286.1450298182074,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-9",
+    type: "serviceComponent",
+    position: {
+      x: 272.87150360585343,
+      y: 207.58186210357036,
+    },
+    data: {
+      url: "aws-asset/Security-Identity-Compliance/Amazon-Cognito.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 272.87150360585343,
+      y: 207.58186210357036,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-10",
+    type: "serviceComponent",
+    position: {
+      x: 274.40435444764876,
+      y: 283.63204954742906,
+    },
+    data: {
+      url: "aws-asset/App-Integration/Amazon-API-Gateway.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 274.40435444764876,
+      y: 283.63204954742906,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-11",
+    type: "serviceComponent",
+    position: {
+      x: 358.9842982433496,
+      y: 367.6729586645732,
+    },
+    data: {
+      url: "aws-asset/Analytics/Amazon-Kinesis-Firehose.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 358.9842982433496,
+      y: 367.6729586645732,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-12",
+    type: "serviceComponent",
+    position: {
+      x: 378.0475128910977,
+      y: 216.89596703750104,
+    },
+    data: {
+      url: "aws-asset/Compute/Amazon-EC2.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 378.0475128910977,
+      y: 216.89596703750104,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-13",
+    type: "serviceComponent",
+    position: {
+      x: 379.55308927374864,
+      y: 283.6208967821272,
+    },
+    data: {
+      url: "aws-asset/Compute/AWS-Lambda.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 379.55308927374864,
+      y: 283.6208967821272,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-14",
+    type: "serviceComponent",
+    position: {
+      x: 264.8797274056039,
+      y: 373.1340472621199,
+    },
+    data: {
+      url: "aws-asset/Storage/Amazon-Simple-Storage-Service.png",
+    },
+    width: 65,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 264.8797274056039,
+      y: 373.1340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-14",
+    type: "textUpdater",
+    position: {
+      x: 264.8797274056039,
+      y: 373.1340472621199,
+    },
+    data: {
+      value: "Telemetry",
+      nodeId: "node-14",
+    },
+    width: 65,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 264.8797274056039,
+      y: 373.1340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-15",
+    type: "textUpdater",
+    position: {
+      x: 326.3797274056039,
+      y: 402.6340472621199,
+    },
+    data: {
+      value: "Kinesis Firehose",
+      nodeId: "node-15",
+    },
+    width: 108,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 326.3797274056039,
+      y: 402.6340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-16",
+    type: "textUpdater",
+    position: {
+      x: 13.379727405603887,
+      y: 351.6340472621199,
+    },
+    data: {
+      value: "IOT Devices",
+      nodeId: "node-16",
+    },
+    width: 81,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 13.379727405603887,
+      y: 351.6340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-17",
+    type: "textUpdater",
+    position: {
+      x: 96.87972740560389,
+      y: 322.1340472621199,
+    },
+    data: {
+      value: "Studio Host",
+      nodeId: "node-17",
+    },
+    width: 81,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 96.87972740560389,
+      y: 322.1340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-18",
+    type: "textUpdater",
+    position: {
+      x: 48.87972740560389,
+      y: 209.6340472621199,
+    },
+    data: {
+      value: "Studio",
+      nodeId: "node-18",
+    },
+    width: 62,
+    height: 21,
+    selected: false,
+    positionAbsolute: {
+      x: 48.87972740560389,
+      y: 209.6340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-19",
+    type: "serviceComponent",
+    position: {
+      x: 530.879727405604,
+      y: 284.6340472621199,
+    },
+    data: {
+      url: "aws-asset/Storage/Amazon-Simple-Storage-Service.png",
+    },
+    width: 30,
+    height: 30,
+    selected: false,
+    positionAbsolute: {
+      x: 530.879727405604,
+      y: 284.6340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-20",
+    type: "textUpdater",
+    position: {
+      x: 528.879727405604,
+      y: 319.6340472621199,
+    },
+    data: {
+      value: "S3",
+      nodeId: "node-20",
+    },
+    width: 31,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 528.879727405604,
+      y: 319.6340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-21",
+    type: "textUpdater",
+    position: {
+      x: 252.87972740560383,
+      y: 315.1340472621199,
+    },
+    data: {
+      value: "API Gateway",
+      nodeId: "node-21",
+    },
+    width: 81,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 252.87972740560383,
+      y: 315.1340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-22",
+    type: "textUpdater",
+    position: {
+      x: 258.37972740560383,
+      y: 241.1340472621199,
+    },
+    data: {
+      value: "Cognito",
+      nodeId: "node-22",
+    },
+    width: 59,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 258.37972740560383,
+      y: 241.1340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-23",
+    type: "textUpdater",
+    position: {
+      x: 374.8797274056039,
+      y: 248.1340472621199,
+    },
+    data: {
+      value: "EC2",
+      nodeId: "node-23",
+    },
+    width: 37,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 374.8797274056039,
+      y: 248.1340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-24",
+    type: "textUpdater",
+    position: {
+      x: 369.3797274056039,
+      y: 321.6340472621199,
+    },
+    data: {
+      value: "Lambda",
+      nodeId: "node-24",
+    },
+    width: 53,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 369.3797274056039,
+      y: 321.6340472621199,
+    },
+    dragging: false,
+  },
+  {
+    id: "node-25",
+    type: "textUpdater",
+    position: {
+      x: 513.879727405604,
+      y: 263.6340472621199,
+    },
+    data: {
+      value: "Data Lake",
+      nodeId: "node-25",
+    },
+    width: 65,
+    height: 17,
+    selected: false,
+    positionAbsolute: {
+      x: 513.879727405604,
+      y: 263.6340472621199,
+    },
+    dragging: false,
   },
 ];
 const App = () => {
@@ -197,6 +830,8 @@ const App = () => {
   });
 
   const onClickMain = useCallback((e) => {
+    console.log(nodes);
+    console.log(edges);
     e.target.id.split("|")[0] === "textUpdater"
       ? dispatch({
           type: "SET_CURRENT_NODE_ID",
