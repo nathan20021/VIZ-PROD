9db9c27e22f3a0efcff1fe74e76c257ce7e86622
Nathan Luong <nathan@jamlabs.com>
2022-12-28 19:04:12 -0500

    Change default page

--- src/reducers/HeaderReducer.js
+++ src/reducers/HeaderReducer.js
@@ -1,4 +1,4 @@
-const HeaderReducer = (state = "untitled", action) => {
+const HeaderReducer = (state = "My AWS Diagram", action) => {
   switch (action.type) {
     case "SET_Header":
       if (action.payload.length < 32) return action.payload;
